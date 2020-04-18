5b875cf30800796aa80594054ebedc5094c85a0e
Cov Developer <shusten@hotmail.com>
2020-04-18 05:19:33 -0300

    feat Search Endpoints completed

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::CategoriesController < ApplicationController
-    def index
-        @categories = Category.all
+    def show
+        @category = Category.find(params[:id])
     end
 end
