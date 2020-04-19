46d6c93cca5b78d78f0e2050e05a5354d0aaf1af
Cov Developer <shusten@hotmail.com>
2020-04-19 20:23:04 -0300

    feat: added Favorites Endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   get 'home/index'
   root "home#index"
 
+  concern :favoritable do |options|
+    shallow do 
+      post "/favorite", { to: "favorites#create", on: :member }.merge(options)
+      delete "/favorite", { to: "favorites#destroy", on: :member }.merge(options)
+    end
+  end
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :dashboard, only: :index
@@ -10,8 +17,12 @@ Rails.application.routes.draw do
       resources :sarch, only: :index
       resources :albums, only: :show do
         resources :recently_heards, only: :create
-        
+      end
+        resources :favorites, only: :index 
+
+        resources :songs, only: [] do
+          concerns :favoritable, favoritable_type: 'Song'
+        end
       end
     end
   end
-end
