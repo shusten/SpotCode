bab10f7cd10be42f81462520e9277c6d2ed692c2
Cov Developer <shusten@hotmail.com>
2020-04-19 03:13:16 -0300

    chore: added albums route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       resources :dashboard, only: :index
       resources :categories, only: [:index, :show]  
       resources :sarch, only: :index
+      resources :albums, only: :index
     end
   end
 end
