e823d26b724d00504e76078e33314ab85655a8eb
Cov Developer <shusten@hotmail.com>
2020-04-20 01:16:07 -0300

    chore: React folder structure created

---
+++ app/javascript/screens/album/index.js
@@ -0,0 +1,11 @@
+import React, { Fragment, useState } from 'react';
+
+        const AlbumScreen = () => {
+            return(
+                <Fragment>
+                    <div>AlbumScreen</div>
+                </Fragment>
+            );
+        }
+                
+             export default AlbumScreen;
