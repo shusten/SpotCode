c3c91e4478c0ad968f331fd1379406cb71ca790f
Cov Developer <shusten@hotmail.com>
2020-04-20 05:07:21 -0300

    feat: added routes

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,13 +1,22 @@
 import React, { Fragment } from 'react';
+
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import { Button } from 'react-bulma-components';
 import 'app.scss';
 
+import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
+
+
 const App = () => {
   return(
     <Fragment>
-     <Button color="primary">My Bulma button</Button>
+        <BrowserRouter>
+            <Routes/>
+        </BrowserRouter>
     </Fragment>
   );
 }
-export default App;
+        
+    
+    export default App;
