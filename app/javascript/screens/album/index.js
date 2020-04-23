2668d2d916468e7be1d6f30fa9efc65e0f127490
Cov Developer <shusten@hotmail.com>
2020-04-22 21:15:21 -0300

    feat: added discovery page

--- app/javascript/screens/album/index.js
+++ app/javascript/screens/album/index.js
@@ -1,12 +1,13 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import NavbarFooter from '../../components/common/navbar_footer';
 
-        const AlbumScreen = () => {
-            return(
-                <Fragment>
-                    <NavbarFooter />
-                </Fragment>
-            );
-        }
-                
-             export default AlbumScreen;
+const AlbumScreen = () => {
+    return(
+        <Fragment>
+            <div>Album Screen</div>
+            <NavbarFooter/>
+        </Fragment>
+    )
+}
+
+export default AlbumScreen;
