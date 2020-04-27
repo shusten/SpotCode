07c921e92ab63cbb667bb9058441105765a61092
Cov Developer <shusten@hotmail.com>
2020-04-27 00:29:26 -0300

    feat: added Favorites"

--- app/javascript/components/common/favorite/index.js
+++ app/javascript/components/common/favorite/index.js
@@ -1,9 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import { FaRegHeart, FaHeart } from "react-icons/fa";
+import FavoritesService from '../../../services/favorites';
 
 const Favorite = (props) => {
-    return (
-        <Fragment>
+    const [favored, setFavored] = useState(props.favored);
+    
+    let FavoredButton;
+    if(favored)
+            FavoredButton = <FaHeart size='25px' className='has-text-white' onClick={() => disfavor()} />
+        else
+            FavoredButton = <FaRegHeart size='25px' className='has-text-white' onClick={() => favor()} /> 
 
+    async function disfavor() {
+        await FavoritesService.delete(props.kind, props.id);
+        setFavored(false);
+    }
+
+    async function favor() {
+        await FavoritesService.create(props.kind, props.id);
+        setFavored(true);
+    }
+
+    return ( 
+        <Fragment> 
+             { FavoredButton }
         </Fragment>
     )
 }
