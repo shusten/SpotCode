af3717bbfcf0f04a47e04ef238586d41fe577fe4
Cov Developer <shusten@hotmail.com>
2020-04-27 02:16:48 -0300

    chore: created searches by text and category

--- app/javascript/components/search/index.js
+++ app/javascript/components/search/index.js
@@ -2,8 +2,28 @@ import React, { Fragment, useEffect, useState } from 'react';
 import SearchBar from './search_bar';
 import Categories from '../common/categories';
 import { Columns } from 'react-bulma-components';
+import SearchService from '../../services/search'
+import CategoriesSearch from '../../services/categories'
 
 const Search = () => {
+
+const [albums, setAblums] = useState([]);
+const [artists, setArtists] = useState([]);
+const [songs, setSongs] = useState([]);
+
+async function fetchCategorySearch(id) {
+    const response = await CategoriesSearch.show(id);
+    setAblums(response.data['albums']);
+    setArtists(response.data['artists']);
+    setSongs(response.data['songs']);
+  }
+
+  async function fetchSearch(query) {
+    const response = await SearchService.index(query);
+    setAblums(response.data['albums']);
+    setArtists(response.data['artists']);
+    setSongs(response.data['songs']);
+  }
     return (
         <Fragment>
             <Columns>
