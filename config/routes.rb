4f938202a8e848e69beeccdefd6195703b9ba82b
Cov Developer <shusten@hotmail.com>
2020-04-29 17:02:47 -0300

    fix: Query return problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,37 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
-  root "home#index"
-
-  concern :favoritable do |options|
-    shallow do 
-      post "/favorite", { to: "favorites#create", on: :member }.merge(options)
-      delete "/favorite", { to: "favorites#destroy", on: :member }.merge(options)
+  namespace :api do
+    namespace :v1 do
+      get 'artists/home'
     end
   end
+  devise_for :users
+  get 'home/index'
+  root 'home#index'
+
+
+  concern :favoritable do |options|
+    shallow do
+      post '/favorite', { to: "favorites#create", on: :member }.merge(options)
+      delete '/favorite',  { to: "favorites#destroy", on: :member }.merge(options)
+    end
+  end 
+
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :dashboard, only: :index
-      resources :categories, only: [:index, :show]  
-      resources :sarch, only: :index
+      resources :categories, only: [:index, :show]
+      resources :search, only: :index
+      resources :artists, only: [:show, :index]
       resources :albums, only: :show do
         resources :recently_heards, only: :create
       end
-        resources :favorites, only: :index 
+      resources :favorites, only: :index
 
-        resources :songs, only: [] do
-          concerns :favoritable, favoritable_type: 'Song'
-        end
+      resources :songs, only: [] do
+        concerns :favoritable, favoritable_type: 'Song'
       end
     end
-    get "*path", to: "home#index", :constraints => lambda{|req| req.path !~ /\.(png|jpg|js|css|json|mp3)$/ }
   end
+  get "*path", to: "home#index", :constraints => lambda{|req| req.path !~ /\.(png|jpg|js|css|json|mp3)$/ }
+end
