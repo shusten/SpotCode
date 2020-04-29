72a066be535660dea85063462d3978c0aa2a1964
Cov Developer <shusten@hotmail.com>
2020-04-29 00:00:01 -0300

    fix: "undefined method each for nil:NilClass" in category controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::CategoriesController < ApplicationController
-    def show
+    def index
+        @categories = Category.all
+      end
+      def show
         @category = Category.find(params[:id])
+      end
     end
-end
