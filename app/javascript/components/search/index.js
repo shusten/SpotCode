65c284ebbb2639a1f931961d2dcb8056c36dfe12
Cov Developer <shusten@hotmail.com>
2020-04-29 22:54:09 -0300

    chore: added results_tabs to search

--- app/javascript/components/search/index.js
+++ app/javascript/components/search/index.js
@@ -4,6 +4,7 @@ import Categories from '../common/categories';
 import { Columns } from 'react-bulma-components';
 import SearchService from '../../services/search';
 import CategoriesService from '../../services/categories';
+import ResultsTabs from '../common/results_tabs';
 
     const Search = () => {
         const [albums, setAlbums] = useState([]);
@@ -31,6 +32,7 @@ async function fetchCategorySearch(id) {
                 </Columns.Column>
         </Columns>
 
+        <ResultsTabs albums={albums} artists={artists} songs={songs} />
         <Categories fetchCategorySearch={fetchCategorySearch}/>
     </Fragment>
   );
